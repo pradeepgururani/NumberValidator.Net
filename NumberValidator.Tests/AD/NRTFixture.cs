5d5eee4e77263f392d398d35b74c401fb1e33b8b
Mayank271999 <81458461+Mayank271999@users.noreply.github.com>
2021-04-02 18:21:09 +0530

    New work (#2)
    Implemented AD.NRT validation.

---
+++ NumberValidator.Tests/AD/NRTFixture.cs
@@ -0,0 +1,44 @@
+﻿using NumberValidator.Helpers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NumberValidator.Tests.DK
+{
+    public class NRTFixture
+    {
+        private readonly Validators.DK.NRT _sut = new Validators.DK.NRT();
+
+        [Fact]
+        void ShouldBeInvalidForLessThan8Digits()
+            => Assert.Throws<InvalidFormatException>(() => _sut.Validate("1234567"));
+
+        [Fact]
+        void ShouldBeInvalidFormatFirstandlastcharactermustbletters()
+            => Assert.Throws<InvalidFormatException>(() => _sut.Validate("A1234567"));
+
+        [Fact]
+        void ShouldBeInvalidFormatFirstlettermustBe()
+            => Assert.Throws<InvalidComponentException>(() => _sut.Validate("X059888N"));
+
+        [Fact]
+        void ShouldBeValidForother()
+            => _sut.IsValid("U132950X").Should().BeTrue();
+
+        [Fact]
+        void ShouldBeInValidcode()
+           => _sut.IsValid("U13925000X").Should().BeFalse();
+
+        [Fact]
+        void ShouldBeInvalidFormatNumberHigher()
+           => Assert.Throws<InvalidComponentException>(() => _sut.Validate("F700000A"));
+
+        [Fact]
+        void ShouldBeInvalidFormatNumberBetween()
+           => Assert.Throws<InvalidComponentException>(() => _sut.Validate("A800000X"));
+
+    }
+}
+       
