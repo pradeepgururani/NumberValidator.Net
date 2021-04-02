43ba202d13ddb80ca27b81851e8c44286fac6d45
Mayank <mayankdeopamd29@gmail.com>
2021-04-02 08:51:34 +0530

    Revert "Revert "this is my first commit and first project""
    This reverts commit e7f8a79bbe0bd0790e6134e2fa04a45cb3274568.

---
+++ NumberValidator.Tests/AD/NRTFixture.cs
@@ -0,0 +1,34 @@
+﻿using NumberValidator.Helpers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NumberValidator.Tests.DK
+{
+    public class NRTFixture
+    {
+        private readonly Validators.DK.NRT _sut = new Validators.DK.NRT();
+
+        [Fact]
+        void ShouldBeInvalidForLessThan8Digits()
+            => Assert.Throws<InvalidFormatException>(() => _sut.Validate("1234567"));
+
+        [Fact]
+        void ShouldBeInvalidFormatFirstandlastcharactermustbletters()
+            => Assert.Throws<InvalidFormatException>(() => _sut.Validate("A1234567"));
+
+        [Fact]
+        void ShouldBeInvalidFormatFirstlettermustBe()
+            => Assert.Throws<InvalidComponentException>(() => _sut.Validate("X059888N"));
+
+        [Fact]
+        void ShouldBeValidForother()
+            => _sut.IsValid("U132950X").Should().BeTrue();
+
+        
+
+    }
+}
+       
