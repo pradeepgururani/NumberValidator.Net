56884e07d7cc0396d4c885c3e46c7f2bdf59bc65
Mayank <mayankdeopamd29@gmail.com>
2021-04-12 17:09:01 +0530

    cleaned up linting/style issues

--- NumberValidator/Validators/AD/NRT.cs
+++ NumberValidator/Validators/AD/NRT.cs
@@ -19,20 +19,20 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 // 02110-1301 USA
 
-//"""NRT (Número de Registre Tributari, Andorra tax number).
-//The Número de Registre Tributari (NRT) is an identifier of legal and natural
-//entities for tax purposes.
-//This number consists of one letter indicating the type of entity, then 6
-//digits, followed by a check letter.
-//More information:
-//*https://www.oecd.org/tax/automatic-exchange/crs-implementation-and-assistance/tax-identification-numbers/Andorra-TIN.pdf
+// """NRT (Número de Registre Tributari, Andorra tax number).
+// The Número de Registre Tributari (NRT) is an identifier of legal and natural
+// entities for tax purposes.
+// This number consists of one letter indicating the type of entity, then 6
+// digits, followed by a check letter.
+// More information:
+// *https://www.oecd.org/tax/automatic-exchange/crs-implementation-and-assistance/tax-identification-numbers/Andorra-TIN.pdf
 
 
 
 using NumberValidator.Helpers;
 using System.Text.RegularExpressions;
 
-namespace NumberValidator.Validators.DK
+namespace NumberValidator.Validators.AD
 {
     public class NRT : IValidator
     {
@@ -55,10 +55,17 @@ namespace NumberValidator.Validators.DK
         /// </summary>
         /// <param name="nrt"></param>
         /// <returns>True if NRT is valid</returns>
+       
         public void Validate(string nrt)
         {
 
-            if ((nrt.Length != 8) || (!char.IsLetter(nrt[0]) || !char.IsLetter(nrt[nrt.Length - 1])))
+            if (nrt.Length != 8)
+            {
+                throw new InvalidLengthException();
+            }
+             
+            if (!char.IsLetter(nrt[0]) ||
+                !char.IsLetter(nrt[nrt.Length - 1]))
             {
                 throw new InvalidFormatException();
             }
