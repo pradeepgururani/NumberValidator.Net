bfe34a9b66867e7577919b07a906b817909509a9
isranikrishna <kreyisrani@gmail.com>
2021-04-14 16:25:41 +0530

    Implemented iban for norway

---
+++ NumberValidator.Tests/NO/IBANFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using NumberValidator.Helpers;
+using Xunit;
+
+namespace NumberValidator.Tests.NO.IBAN
+{
+    public class IBANFixture
+    {
+        private readonly Validators.NO.IBAN _sut = new Validators.NO.IBAN();
+
+        [Fact]
+        void ShouldBeInvalidForLessThan15Digits()
+            => _sut.Invoking(_ => _.Validate("123456789")).Should().Throw<InvalidLengthException>();
+
+        [Fact]
+        void ValidIbanShouldBeTrue()
+            => _sut.IsValid("NO9386011117947").Should().BeTrue();
+
+        [Fact]
+        void InValidIbanShouldBeFalse()
+            => _sut.IsValid("GR1601101050000010547023795").Should().BeFalse();
+
+    }
+}
---
+++ NumberValidator/Validators/NO/IBAN.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using NumberValidator.Helpers;
+
+namespace NumberValidator.Validators.NO
+{
+    public class IBAN : IValidator
+    {
+        public bool IsValid(string iban)
+        {
+            try
+            {
+                Validate(iban);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private string ToKontonr(string iban)
+        {
+            
+
+            return (iban.Substring(4, 11));
+        }
+
+        public void Validate(string iban)
+        {
+            
+        
+            if (iban.Length != 15)
+            {
+                throw new InvalidLengthException();
+            }
+
+            if (!iban.StartsWith("NO"))
+            {
+                throw new InvalidComponentException();
+            }
+
+            if(!ToKontonr(iban).IsDigits() ||
+                ToKontonr(iban).Length != 11)
+            {
+                throw new InvalidFormatException();
+            }
+        }
+    }
+}
