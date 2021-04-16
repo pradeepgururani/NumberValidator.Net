de39fcade43d69b40e44cd34359ef02c32ab160b
Murari Yadav <murari.yadav@thingmzing.com>
2021-04-16 11:01:52 +0530

    first commit

---
+++ NumberValidator.Tests/AT/BusinessidFixture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentAssertions;
+using NumberValidator.Helpers;
+using Xunit;
+
+namespace NumberValidator.Tests.AT
+{
+    public class BusinessidFixture
+    {
+        private readonly Validators.AT.BUSINESSID _sut = new Validators.AT.BUSINESSID();
+
+        [Fact]
+        void ValidBusinesidShouldBeTrue()
+                => _sut.IsValid("122119m").Should().BeTrue();
+        [Fact]
+        void InValidBusinessShouldBeFalse()
+           => _sut.IsValid("FN 122119m").Should().BeFalse();
+
+    }
+}
---
+++ NumberValidator/Validators/AT/businessid.cs
@@ -0,0 +1,35 @@
+﻿using NumberValidator.Helpers;
+using System.Text.RegularExpressions;
+
+namespace NumberValidator.Validators.AT
+
+{
+    public class BUSINESSID : IValidator
+    {
+        
+
+        public bool IsValid(string businessid)
+        {
+           
+            try
+            {
+                Validate(businessid);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public void Validate(string businessid)
+        {
+            // var bid = new Regex("^[0-9]+[a-z]$");
+            //businessid = businessid.RemoveSpecialCharacthers().Replace("FN", string.Empty).Replace("fn", string.Empty);
+            if (!Regex.IsMatch(businessid, "^[0-9]+[a-z]$"))
+            {
+                throw new InvalidFormatException();
+            }
+            return;
+        }
+    }
+}
