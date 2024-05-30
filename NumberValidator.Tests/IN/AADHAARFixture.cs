4bd097192d7a3f0be8db18000acc79ff0f2567f6
Kr.Saumik <87721438+KrSaumik@users.noreply.github.com>
2024-05-30 18:49:55 +0530

    Implemented IN AADHAAR validation and tests. (#13)
    * Implemented IN AADHAAR validation and tests.
    
    * fixed issues
    
    * fixed naming conventions
    
    * minor changes
    
    ---------
    
    Co-authored-by: KrSaumik <Kumar.Saumik@cogoport.com>

---
+++ NumberValidator.Tests/IN/AADHAARFixture.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using FluentAssertions;
+using NumberValidator.Helpers;
+
+namespace NumberValidator.Tests.IN
+{
+    public class AadhaarFixture
+    {
+        private readonly Validators.IN.Aadhaar _sut = new Validators.IN.Aadhaar();
+
+        [Fact]
+        void ShouldBeInvalidForLessThan12Digits()
+            => Assert.Throws<InvalidLengthException>(() => _sut.Validate("22345678"));
+
+        [Fact]
+        void ShouldBeInvalidFormatCanNotBePalindrome()
+            => Assert.Throws<InvalidFormatException>(() => _sut.Validate("223567765322"));
+
+        [Fact]
+        void ShouldBeInvalidFormatPatternNotFollowed()
+            => Assert.Throws<InvalidFormatException>(() => _sut.Validate("128565765309"));
+
+        [Fact]
+        void ShouldBeValid()
+            => _sut.IsValid("234675789836").Should().BeTrue();
+    }
+}
---
+++ NumberValidator/Validators/IN/AADHAAR.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using NumberValidator.Helpers;
+using System.Text.RegularExpressions;
+
+namespace NumberValidator.Validators.IN
+{
+    public class Aadhaar : IValidator
+    {
+        public bool IsValid(string aadhaar)
+        {
+            try
+            {
+                Validate(aadhaar);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void Validate(string aadhaar)
+        {
+            var aadhaarPattern = @"^[2-9][0-9]{11}$";
+
+            if (aadhaar.Length != 12)
+            {
+                throw new InvalidLengthException();
+            }
+
+            if (aadhaar.SequenceEqual(aadhaar.Reverse()))
+            {
+                throw new InvalidFormatException();
+            }
+
+            var aadhaarRegex = new Regex(aadhaarPattern);
+
+            if (!aadhaarRegex.IsMatch(aadhaar))
+            {
+                throw new InvalidFormatException();
+            }
+        }
+    }
+}
