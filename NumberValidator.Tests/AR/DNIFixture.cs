f04986b7e45abfeba101807bfba3c6b4dec249dc
Thenila <131799718+Thenila@users.noreply.github.com>
2024-08-21 15:12:40 +0530

    Implemented AR DNI (#18)
    * Implemented AR DNI
    
    * updated
    
    * updated

---
+++ NumberValidator.Tests/AR/DNIFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using NumberValidator.Helpers;
+using Xunit;
+using NumberValidator.Validators.AR;
+
+namespace NumberValidator.Tests.AR
+{
+    public class DNIFixture
+    {
+        private readonly DNI _sut=new DNI();
+        [Fact]
+        void ShouldBeInvalidForLessThan8Digits()
+            => Assert.Throws<InvalidLengthException>(() => _sut.Validate("012345"));
+
+        [Fact]
+        void ShouldBeInvalidForMoreThan8Digits()
+            => Assert.Throws<InvalidLengthException>(() => _sut.Validate("0123456789"));
+
+        [Fact]
+        void ShouldBeValidForOthers()
+            => Assert.Throws<InvalidLengthException>(() => _sut.Validate("0123456"));
+
+    }
+}
